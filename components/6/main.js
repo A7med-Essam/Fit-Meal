5307fe1d851ef5a24fce142d0176d2346ab0a427
Ahmed Essam <70452432+A7med-Essam@users.noreply.github.com>
2023-02-13 16:34:53 +0200

    first commit

---
+++ components/6/main.js
@@ -0,0 +1,15 @@
+$('#menuCards .owl-carousel').owlCarousel({
+    loop:true,
+    margin:30,
+    responsive:{
+        0:{
+            items:1
+        },
+        800:{
+            items:2
+        },
+        1200:{
+            items:3
+        }
+    }
+})
